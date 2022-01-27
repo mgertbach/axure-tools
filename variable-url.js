1511c7e2d7ad0b2e72ea9d93ac0dc3a6e35f5b65
Molly Gertenbach <mgertbach@gmail.com>
2022-01-27 13:18:33 -0500

    position frame

--- variable-url.js
+++ variable-url.js
@@ -9,7 +9,10 @@ var input = $(parentbody).find("#pageURL");
 var urlHTML = "<div id='pageURL'> \n\
 <style> \n\
 #mainPanelContainer { \n\
-  top: 0 !important; \n\
+  position: absolute !important; \n\
+  top: 65px !important; \n\
+  left: 50% !important; \n\
+  transform: translateX(-50%) !important; \n\
 } \n\
 #pageInput { \n\
   margin: 10px; \n\
